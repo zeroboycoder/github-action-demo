fd36a03a095c4b9d55bcd7cff0629b987dd7c5a1
zeroboycoder <pyaesonekhant@tumawlamyine.edu.mm>
2023-06-19 17:56:32 +0630

    Add one route

--- app.js
+++ app.js
@@ -3,5 +3,7 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Hello World"));
 
+app.get("/name", (req, res) => res.send("ZEROBOY"));
+
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server is runnin at port : ${PORT}`));
